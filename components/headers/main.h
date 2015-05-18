c02bc7ecfee4c349d6daa6f0fdef635c71089e79
Ravi Kumar L <upman16@gmail.com>
2015-05-18 08:10:47 +0530

    Add include guard for main.h

--- components/headers/main.h
+++ components/headers/main.h
@@ -1,15 +1,18 @@
-void resetWorld();
-void menu();
-void createMenu();
-void renderWorld();
-void renderBackground();
-void display();
-void keyboard(unsigned char, int, int);
-void mouse(int, int, int, int);
-void motion(int x, int y);
-void step();
-void init();
-void switchToSimulation();
-void switchToIntro();
-void introKeyboard(unsigned char, int, int);
-void scaleSelectedBody(float);
+#ifndef MAIN_H_
+#define MAIN_H_
+  void resetWorld();
+  void menu();
+  void createMenu();
+  void renderWorld();
+  void renderBackground();
+  void display();
+  void keyboard(unsigned char, int, int);
+  void mouse(int, int, int, int);
+  void motion(int x, int y);
+  void step();
+  void init();
+  void switchToSimulation();
+  void switchToIntro();
+  void introKeyboard(unsigned char, int, int);
+  void scaleSelectedBody(float);
+#endif
